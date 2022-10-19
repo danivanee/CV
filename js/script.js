6aeb7b2c9ca8d95f6854526a60350747c9a39fdf
danivanee <danivanee_170@hotmail.com>
2022-10-19 13:28:12 -0500

    Se agrego funcionalidad al api de chuck norris

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 const but = document.querySelector('button');
-const parrafo = document.querySelector('h5');
-but.addEventListener('click', getData);
+const parrafo = document.querySelector('h6');
+
+but.addEventListener('click', getData)
+
 
 async function getData(){
     try{
     const data = await fetch('https://api.chucknorris.io/jokes/random');
     const json = await data.json();
-   // console.log(json);
+    
+    
+    //console.log(json);
     //console.log(json.value);
     parrafo.textContent = json.value;
+
+    
     
 }
 catch(e){
     console.error(e);
 }
+
 }
