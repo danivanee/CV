c670c0ef0f27d5a5bbf98da4dd852f4be2fc7fd3
danivanee <danivanee_170@hotmail.com>
2022-10-18 20:59:25 -0500

    Se agrego boton para traer el api de Chuck Norris

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const but = document.querySelector('button');
+const parrafo = document.querySelector('h5');
+but.addEventListener('click', getData);
+
+async function getData(){
+    try{
+    const data = await fetch('https://api.chucknorris.io/jokes/random');
+    const json = await data.json();
+   // console.log(json);
+    //console.log(json.value);
+    parrafo.textContent = json.value;
+    
+}
+catch(e){
+    console.error(e);
+}
+}
