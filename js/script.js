8fca0d8eb98e912de5e7a5458f887a1eaf1f5d13
danivanee <danivanee_170@hotmail.com>
2022-10-19 20:03:49 -0500

    Correccion de funcionalidad de api response

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 const but = document.querySelector('button');
-const parrafo = document.querySelector('h6');
+const parrafo = document.getElementById('apiResponse');
 
 but.addEventListener('click', getData)
 
+const url = 'https://api.chucknorris.io/jokes/random'
 
 async function getData(){
     try{
-    const data = await fetch('https://api.chucknorris.io/jokes/random');
+    const data = await fetch(url);
     const json = await data.json();
     
     
     //console.log(json);
     //console.log(json.value);
     parrafo.textContent = json.value;
+    return parrafo.textContent
 
     
     
